0f2e00639e7f3e6ee3637435324fa41d2b7afeac
Balázs <balazs@balazs-Inspiron-N7010.(none)>
2012-12-05 17:43:10 +0100

    új adatbázis
    Signed-off-by: Balázs <balazs@balazs-Inspiron-N7010.(none)>

--- 7merfold_development.sql
+++ 7merfold_development.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Hoszt: localhost
--- Létrehozás ideje: 2012. dec. 05. 15:57
+-- Létrehozás ideje: 2012. dec. 05. 17:42
 -- Szerver verzió: 5.5.16
 -- PHP verzió: 5.3.8
 
@@ -40,7 +40,7 @@ CREATE TABLE IF NOT EXISTS `categories` (
 --
 
 INSERT INTO `categories` (`id`, `slug`, `name`, `created`) VALUES
-(1, 'varosi-kalandok', 'Városi kalandok', '2012-12-05 14:45:27'),
+(1, 'varosi-kalandok', 'Városi kalandok', '2012-12-05 16:41:19'),
 (2, 'korutazasok', 'Körutazások', '0000-00-00 00:00:00'),
 (3, 'nyaralasok-uveghegyen-tul', 'Üveghegyen túl', '0000-00-00 00:00:00'),
 (4, 'nyaralasok-uveghegyen-innen', 'Üveghegyen innen', '0000-00-00 00:00:00'),
@@ -58,6 +58,7 @@ CREATE TABLE IF NOT EXISTS `countries` (
   `useful_information` text COLLATE utf8_hungarian_ci NOT NULL COMMENT 'Érdekességek és egyéb országgal kapcsolatos hasznos információk',
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `name` varchar(255) COLLATE utf8_hungarian_ci NOT NULL,
+  `visa_info` text COLLATE utf8_hungarian_ci,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci AUTO_INCREMENT=3 ;
 
@@ -65,9 +66,73 @@ CREATE TABLE IF NOT EXISTS `countries` (
 -- A tábla adatainak kiíratása `countries`
 --
 
-INSERT INTO `countries` (`id`, `information`, `useful_information`, `created`, `name`) VALUES
-(1, 'Görögország', 'Görögország hasznos', '0000-00-00 00:00:00', 'Görögország'),
-(2, 'Franciaország leírása', 'Franciaország haszno', '0000-00-00 00:00:00', 'Franciaország');
+INSERT INTO `countries` (`id`, `information`, `useful_information`, `created`, `name`, `visa_info`) VALUES
+(1, 'Görögország', 'Görögország hasznos', '0000-00-00 00:00:00', 'Görögország', 'Vízum Görögországba'),
+(2, 'Franciaország leírása', 'Franciaország haszno', '0000-00-00 00:00:00', 'Franciaország', 'Vízum Franciaországba');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet: `hotels`
+--
+
+CREATE TABLE IF NOT EXISTS `hotels` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_hungarian_ci NOT NULL,
+  `star_rating` int(11) NOT NULL,
+  `description` text COLLATE utf8_hungarian_ci,
+  `accommodation` text COLLATE utf8_hungarian_ci NOT NULL,
+  `price` varchar(255) COLLATE utf8_hungarian_ci NOT NULL,
+  `trip_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `trip_id` (`trip_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet: `news`
+--
+
+CREATE TABLE IF NOT EXISTS `news` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_hungarian_ci NOT NULL,
+  `content` text COLLATE utf8_hungarian_ci NOT NULL,
+  `image_file` varchar(255) COLLATE utf8_hungarian_ci NOT NULL,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci AUTO_INCREMENT=3 ;
+
+--
+-- A tábla adatainak kiíratása `news`
+--
+
+INSERT INTO `news` (`id`, `title`, `content`, `image_file`, `created`) VALUES
+(1, 'Sokan szeretnek nyaralni!', 'Kutatók megállapították, hogy a nyaralás jó.', '3.jpeg', '0000-00-00 00:00:00'),
+(2, 'A hajó utak menők!', 'Brit tudósok kimutatták, hogy az emberek 98%-a imádja a hajókat. Miért ne nyaralna ön is hajón?', '4.jpeg', '2012-12-05 16:41:20');
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet: `programs`
+--
+
+CREATE TABLE IF NOT EXISTS `programs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8_hungarian_ci NOT NULL,
+  `description` varchar(255) COLLATE utf8_hungarian_ci NOT NULL,
+  `image_file` varchar(255) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `trip_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `trip_id` (`trip_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci AUTO_INCREMENT=2 ;
+
+--
+-- A tábla adatainak kiíratása `programs`
+--
+
+INSERT INTO `programs` (`id`, `name`, `description`, `image_file`, `trip_id`) VALUES
+(1, '1. nap', 'leírás', NULL, 1);
 
 -- --------------------------------------------------------
 
@@ -90,9 +155,9 @@ CREATE TABLE IF NOT EXISTS `regions` (
 --
 
 INSERT INTO `regions` (`id`, `country_id`, `description`, `created`, `name`) VALUES
-(1, 1, 'Kréta leírása', '2012-12-05 14:45:27', 'Kréta'),
+(1, 1, 'Kréta leírása', '2012-12-05 16:41:19', 'Kréta'),
 (2, 1, 'Korfu leírása', '0000-00-00 00:00:00', 'Korfu'),
-(3, 2, 'Korzika leírása', '2012-12-05 14:45:27', 'Korzika'),
+(3, 2, 'Korzika leírása', '2012-12-05 16:41:19', 'Korzika'),
 (4, 2, 'Cote d azur', '0000-00-00 00:00:00', 'Cote d azur');
 
 -- --------------------------------------------------------
@@ -114,7 +179,7 @@ CREATE TABLE IF NOT EXISTS `sights` (
 --
 
 INSERT INTO `sights` (`id`, `name`, `image_file`, `created`) VALUES
-(1, 'Teszt látnivaló', '3.jpeg', '2012-12-05 14:45:27');
+(1, 'Teszt látnivaló', '3.jpeg', '2012-12-05 16:41:19');
 
 -- --------------------------------------------------------
 
@@ -158,6 +223,15 @@ CREATE TABLE IF NOT EXISTS `trips` (
   `hajozz` tinyint(1) NOT NULL DEFAULT '0' COMMENT 'kikerüljön-e a hajozz.eu-ra mutató link?',
   `region_id` int(11) DEFAULT NULL,
   `category_id` int(11) NOT NULL DEFAULT '1',
+  `accommodation` text COLLATE utf8_hungarian_ci,
+  `travel_method` text COLLATE utf8_hungarian_ci,
+  `minimal_persons` int(11) DEFAULT NULL,
+  `extra` text COLLATE utf8_hungarian_ci COMMENT 'Tetszőleges szöveges blokk',
+  `extra_title` varchar(255) COLLATE utf8_hungarian_ci DEFAULT NULL COMMENT 'Tetszőleges szöveges blokk címe',
+  `star_rating` int(11) DEFAULT NULL,
+  `day` text COLLATE utf8_hungarian_ci,
+  `special` text COLLATE utf8_hungarian_ci,
+  `service` text COLLATE utf8_hungarian_ci,
   PRIMARY KEY (`id`),
   KEY `country` (`country_id`),
   KEY `region` (`region_id`),
@@ -168,13 +242,13 @@ CREATE TABLE IF NOT EXISTS `trips` (
 -- A tábla adatainak kiíratása `trips`
 --
 
-INSERT INTO `trips` (`id`, `description`, `name`, `price`, `travel_date`, `image_file`, `updated`, `created`, `short_description`, `travel_price_includes`, `country_id`, `circle_image_file`, `hajozz`, `region_id`, `category_id`) VALUES
-(1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget velit sapien. Curabitur nulla nibh, ornare a posuere nec, hendrerit non elit. Sed vestibulum lacus non est posuere eget adipiscing libero faucibus. Vivamus laoreet lorem a tortor feugiat non viverra velit pharetra. Cras ac tellus et urna blandit sodales vel ut ligula. Suspendisse potenti. Duis id elit elit. Quisque at nisl vel neque ultrices egestas. Vestibulum sit amet arcu ut libero placerat ornare id sed nibh. Sed molestie laoreet fermentum. Morbi aliquet, turpis in tristique pulvinar, mi nibh dictum mi, sed bibendum est ligula nec nulla. ', 'Teszt út 1.', '120000', '2012-11-11', '3.jpeg', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'Teszt út rövid leírása', 'Büféreggeli, transzfer', 1, '3.jpeg', 1, 1, 1),
-(2, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget velit sapien. Curabitur nulla nibh, ornare a posuere nec, hendrerit non elit. Sed vestibulum lacus non est posuere eget adipiscing libero faucibus. Vivamus laoreet lorem a tortor feugiat non viverra velit pharetra. Cras ac tellus et urna blandit sodales vel ut ligula. Suspendisse potenti. Duis id elit elit. Quisque at nisl vel neque ultrices egestas. Vestibulum sit amet arcu ut libero placerat ornare id sed nibh. Sed molestie laoreet fermentum. Morbi aliquet, turpis in tristique pulvinar, mi nibh dictum mi, sed bibendum est ligula nec nulla. ', 'Teszt út 2.', '120000', '2012-11-11', '3.jpeg', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'Teszt út rövid leírása', 'Büféreggeli, transzfer', 1, '3.jpeg', 0, 2, 1),
-(3, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget velit sapien. Curabitur nulla nibh, ornare a posuere nec, hendrerit non elit. Sed vestibulum lacus non est posuere eget adipiscing libero faucibus. Vivamus laoreet lorem a tortor feugiat non viverra velit pharetra. Cras ac tellus et urna blandit sodales vel ut ligula. Suspendisse potenti. Duis id elit elit. Quisque at nisl vel neque ultrices egestas. Vestibulum sit amet arcu ut libero placerat ornare id sed nibh. Sed molestie laoreet fermentum. Morbi aliquet, turpis in tristique pulvinar, mi nibh dictum mi, sed bibendum est ligula nec nulla. ', 'Teszt út 3.', '120000', '2012-11-11', '3.jpeg', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'Teszt út rövid leírása', 'Büféreggeli, transzfer', 2, '3.jpeg', 0, 1, 2),
-(4, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget velit sapien. Curabitur nulla nibh, ornare a posuere nec, hendrerit non elit. Sed vestibulum lacus non est posuere eget adipiscing libero faucibus. Vivamus laoreet lorem a tortor feugiat non viverra velit pharetra. Cras ac tellus et urna blandit sodales vel ut ligula. Suspendisse potenti. Duis id elit elit. Quisque at nisl vel neque ultrices egestas. Vestibulum sit amet arcu ut libero placerat ornare id sed nibh. Sed molestie laoreet fermentum. Morbi aliquet, turpis in tristique pulvinar, mi nibh dictum mi, sed bibendum est ligula nec nulla. ', 'Teszt út 4.', '120000', '2012-11-11', '3.jpeg', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'Teszt út rövid leírása', 'Büféreggeli, transzfer', 2, '3.jpeg', 0, 1, 3),
-(5, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget velit sapien. Curabitur nulla nibh, ornare a posuere nec, hendrerit non elit. Sed vestibulum lacus non est posuere eget adipiscing libero faucibus. Vivamus laoreet lorem a tortor feugiat non viverra velit pharetra. Cras ac tellus et urna blandit sodales vel ut ligula. Suspendisse potenti. Duis id elit elit. Quisque at nisl vel neque ultrices egestas. Vestibulum sit amet arcu ut libero placerat ornare id sed nibh. Sed molestie laoreet fermentum. Morbi aliquet, turpis in tristique pulvinar, mi nibh dictum mi, sed bibendum est ligula nec nulla. ', 'Teszt út 5.', '120000', '2012-11-11', '3.jpeg', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'Teszt út rövid leírása', 'Büféreggeli, transzfer', 2, '3.jpeg', 0, 1, 4),
-(6, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget velit sapien. Curabitur nulla nibh, ornare a posuere nec, hendrerit non elit. Sed vestibulum lacus non est posuere eget adipiscing libero faucibus. Vivamus laoreet lorem a tortor feugiat non viverra velit pharetra. Cras ac tellus et urna blandit sodales vel ut ligula. Suspendisse potenti. Duis id elit elit. Quisque at nisl vel neque ultrices egestas. Vestibulum sit amet arcu ut libero placerat ornare id sed nibh. Sed molestie laoreet fermentum. Morbi aliquet, turpis in tristique pulvinar, mi nibh dictum mi, sed bibendum est ligula nec nulla. ', 'Teszt út 6.', '120000', '2012-11-11', '3.jpeg', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'Teszt út rövid leírása', 'Büféreggeli, transzfer', 2, '3.jpeg', 0, 1, 5);
+INSERT INTO `trips` (`id`, `description`, `name`, `price`, `travel_date`, `image_file`, `updated`, `created`, `short_description`, `travel_price_includes`, `country_id`, `circle_image_file`, `hajozz`, `region_id`, `category_id`, `accommodation`, `travel_method`, `minimal_persons`, `extra`, `extra_title`, `star_rating`, `day`, `special`, `service`) VALUES
+(1, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget velit sapien. Curabitur nulla nibh, ornare a posuere nec, hendrerit non elit. Sed vestibulum lacus non est posuere eget adipiscing libero faucibus. Vivamus laoreet lorem a tortor feugiat non viverra velit pharetra. Cras ac tellus et urna blandit sodales vel ut ligula. Suspendisse potenti. Duis id elit elit. Quisque at nisl vel neque ultrices egestas. Vestibulum sit amet arcu ut libero placerat ornare id sed nibh. Sed molestie laoreet fermentum. Morbi aliquet, turpis in tristique pulvinar, mi nibh dictum mi, sed bibendum est ligula nec nulla. ', 'Teszt út 1.', '120000', '2012-11-11', '3.jpeg', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'Teszt út rövid leírása', 'Büféreggeli, transzfer', 1, '3.jpeg', 1, 1, 1, 'Ellátás az úthoz', 'Repülővel', 12, 'Extra leírás', 'Extra szöveges blokk címe', NULL, NULL, NULL, NULL),
+(2, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget velit sapien. Curabitur nulla nibh, ornare a posuere nec, hendrerit non elit. Sed vestibulum lacus non est posuere eget adipiscing libero faucibus. Vivamus laoreet lorem a tortor feugiat non viverra velit pharetra. Cras ac tellus et urna blandit sodales vel ut ligula. Suspendisse potenti. Duis id elit elit. Quisque at nisl vel neque ultrices egestas. Vestibulum sit amet arcu ut libero placerat ornare id sed nibh. Sed molestie laoreet fermentum. Morbi aliquet, turpis in tristique pulvinar, mi nibh dictum mi, sed bibendum est ligula nec nulla. ', 'Teszt út 2.', '120000', '2012-11-11', '3.jpeg', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'Teszt út rövid leírása', 'Büféreggeli, transzfer', 1, '3.jpeg', 0, 2, 1, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(3, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget velit sapien. Curabitur nulla nibh, ornare a posuere nec, hendrerit non elit. Sed vestibulum lacus non est posuere eget adipiscing libero faucibus. Vivamus laoreet lorem a tortor feugiat non viverra velit pharetra. Cras ac tellus et urna blandit sodales vel ut ligula. Suspendisse potenti. Duis id elit elit. Quisque at nisl vel neque ultrices egestas. Vestibulum sit amet arcu ut libero placerat ornare id sed nibh. Sed molestie laoreet fermentum. Morbi aliquet, turpis in tristique pulvinar, mi nibh dictum mi, sed bibendum est ligula nec nulla. ', 'Teszt út 3.', '120000', '2012-11-11', '3.jpeg', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'Teszt út rövid leírása', 'Büféreggeli, transzfer', 2, '3.jpeg', 0, 1, 2, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(4, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget velit sapien. Curabitur nulla nibh, ornare a posuere nec, hendrerit non elit. Sed vestibulum lacus non est posuere eget adipiscing libero faucibus. Vivamus laoreet lorem a tortor feugiat non viverra velit pharetra. Cras ac tellus et urna blandit sodales vel ut ligula. Suspendisse potenti. Duis id elit elit. Quisque at nisl vel neque ultrices egestas. Vestibulum sit amet arcu ut libero placerat ornare id sed nibh. Sed molestie laoreet fermentum. Morbi aliquet, turpis in tristique pulvinar, mi nibh dictum mi, sed bibendum est ligula nec nulla. ', 'Teszt út 4.', '120000', '2012-11-11', '3.jpeg', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'Teszt út rövid leírása', 'Büféreggeli, transzfer', 2, '3.jpeg', 0, 1, 3, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(5, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget velit sapien. Curabitur nulla nibh, ornare a posuere nec, hendrerit non elit. Sed vestibulum lacus non est posuere eget adipiscing libero faucibus. Vivamus laoreet lorem a tortor feugiat non viverra velit pharetra. Cras ac tellus et urna blandit sodales vel ut ligula. Suspendisse potenti. Duis id elit elit. Quisque at nisl vel neque ultrices egestas. Vestibulum sit amet arcu ut libero placerat ornare id sed nibh. Sed molestie laoreet fermentum. Morbi aliquet, turpis in tristique pulvinar, mi nibh dictum mi, sed bibendum est ligula nec nulla. ', 'Teszt út 5.', '120000', '2012-11-11', '3.jpeg', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'Teszt út rövid leírása', 'Büféreggeli, transzfer', 2, '3.jpeg', 0, 1, 4, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL),
+(6, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam eget velit sapien. Curabitur nulla nibh, ornare a posuere nec, hendrerit non elit. Sed vestibulum lacus non est posuere eget adipiscing libero faucibus. Vivamus laoreet lorem a tortor feugiat non viverra velit pharetra. Cras ac tellus et urna blandit sodales vel ut ligula. Suspendisse potenti. Duis id elit elit. Quisque at nisl vel neque ultrices egestas. Vestibulum sit amet arcu ut libero placerat ornare id sed nibh. Sed molestie laoreet fermentum. Morbi aliquet, turpis in tristique pulvinar, mi nibh dictum mi, sed bibendum est ligula nec nulla. ', 'Teszt út 6.', '120000', '2012-11-11', '3.jpeg', '0000-00-00 00:00:00', '0000-00-00 00:00:00', 'Teszt út rövid leírása', 'Büféreggeli, transzfer', 2, '3.jpeg', 0, 1, 5, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
