e162a264823334785672d2f1d2a68f05f279dedb
Balázs <balazs@balazs-Inspiron-N7010.(none)>
2012-12-12 14:52:21 +0100

    slug függvény
    Signed-off-by: Balázs <balazs@balazs-Inspiron-N7010.(none)>

--- app/Model/AppModel.php
+++ app/Model/AppModel.php
@@ -31,4 +31,62 @@ App::uses('Model', 'Model');
  * @package       app.Model
  */
 class AppModel extends Model {
+
+	public function beforeSave($options = array()) {
+    foreach($this->data as $model => $v){
+    	foreach($this->data[$model] as $field => $value){
+    		if($field == 'title' || $field == 'name'){
+    			$text = $value;
+    		}
+    	}
+    	if(isset($this->data[$model]['slug'])){
+  			$this->data[$model]['slug'] = $this->create_slug($text, $model);
+  		}
+    }
+   	return true;
+	}
+
+	public function create_slug($string, $model){
+		$unique = false;
+		$i = 0;
+		while(!$unique){
+			$slug = $this->slugify($string);
+			$orig = $string;
+			/* Kérdéses model használata */
+			$m = ClassRegistry::init($model);
+			$count = $m->find('count', array('conditions' => array(($model . '.slug') => $slug)));
+			if($count == 0){
+				$unique = true;
+			}else{
+				$i++;
+				$string = $orig . ("-" . $count);
+			}
+		}
+		return $slug;
+	}
+
+	static public function slugify($text)
+	{ 
+	  // replace non letter or digits by -
+	  $text = preg_replace('~[^\\pL\d]+~u', '-', $text);
+
+	  // trim
+	  $text = trim($text, '-');
+
+	  // transliterate
+	  $text = iconv('utf-8', 'us-ascii//TRANSLIT', $text);
+
+	  // lowercase
+	  $text = strtolower($text);
+
+	  // remove unwanted characters
+	  $text = preg_replace('~[^-\w]+~', '', $text);
+
+	  if (empty($text))
+	  {
+	    return 'n-a';
+	  }
+	  return $text;
+	}
+
 }
