409ff07dc688670ff626b2d5cdc36df48dccaaf2
Balázs <balazs@balazs-Inspiron-N7010.(none)>
2012-12-20 15:30:56 +0100

    jav
    Signed-off-by: Balázs <balazs@balazs-Inspiron-N7010.(none)>

--- app/webroot/js/admin.js
+++ app/webroot/js/admin.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 	$('#TripCountryId').change(function() {
 		var country = $('#TripCountryId').val();
-	  $.post('/7merfold/regions/' + country, function(data) {
+	  $.post('/regions/' + country, function(data) {
 	  	  data = $.parseJSON(data);
 	  	  var options = '<option value="">Nincs régió</option>';
 		  $.each(data, function(id, name) {
