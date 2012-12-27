52251547979d5329465768b09c19c530feacc536
Balázs <balazs@balazs-Inspiron-N7010.(none)>
2012-12-27 10:12:25 +0100

    server jav.
    Signed-off-by: Balázs <balazs@balazs-Inspiron-N7010.(none)>

--- app/webroot/js/admin.js
+++ app/webroot/js/admin.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
 	$('#TripCountryId').change(function() {
 		var country = $('#TripCountryId').val();
-	  $.post('/7merfold/regions/' + country, function(data) {
+	  $.post('/regions/' + country, function(data) {
 	  	  data = $.parseJSON(data);
 	  	  var options = '<option value="">Nincs régió</option>';
 		  $.each(data, function(id, name) {
@@ -14,7 +14,7 @@ $(document).ready(function(){
 
 	$('#TripContinentId').change(function() {
 		var continent = $('#TripContinentId').val();
-	  $.post('/7merfold/countries/' + continent, function(data) {
+	  $.post('/countries/' + continent, function(data) {
 	  	  data = $.parseJSON(data);
 	  	  var options = '';
 		  $.each(data, function(id, name) {
