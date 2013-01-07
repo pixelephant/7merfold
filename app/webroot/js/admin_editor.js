7bba86009ac51311365efa284b84c64d656614e9
Balázs <balazs@balazs-Inspiron-N7010.(none)>
2013-01-07 17:01:15 +0100

    ár mező nagyobb
    Signed-off-by: Balázs <balazs@balazs-Inspiron-N7010.(none)>

--- app/webroot/js/admin_editor.js
+++ app/webroot/js/admin_editor.js
@@ -4,5 +4,9 @@ $(document).ready(
 		$('textarea:not(.non-redactor)').redactor({
 			toolbar: 'custom'
 		});
+		setTimeout(function(){
+			$("label[for=TripPrice]").parent().find(".redactor_frame").css("height", "250px");
+		}, 2000);
+		
 	}
 );
